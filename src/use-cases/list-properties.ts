bc54019fd0d2558a588ca10b2dfd1eb0bca6481c
Scopel_Yan <160073076+ScopelYann@users.noreply.github.com>
2026-06-05 17:36:25 -0300

    Update

--- src/use-cases/list-properties.ts
+++ src/use-cases/list-properties.ts
@@ -4,7 +4,7 @@ import { properties } from "@/http/controller/properties/routes";
 export type ListPropertiesUseCaseResponse = {
 	properties: Properties[];
 };
-
+//TODO github
 export class ListPropertiesUseCase {
 	execute(): ListPropertiesUseCaseResponse {
 		return { properties };
